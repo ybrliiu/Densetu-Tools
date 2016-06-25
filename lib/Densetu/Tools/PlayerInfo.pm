e7fb21a4ea995f146ef5aef0db488de592967a20
liiu <yliiu_nan-na@docomo.ne.jp>
2016-06-25 16:46:49 +0900

    output method の機能追加

--- lib/Densetu/Tools/PlayerInfo.pm
+++ lib/Densetu/Tools/PlayerInfo.pm
@@ -21,9 +21,17 @@ package Densetu::Tools::PlayerInfo {
   Class::Accessor::Lite->mk_accessors(qw/id pass/);
 
   sub output {
-    my ($self) = @_;
+    my $self = shift;
 
-    my $output = $self->get_log();
+    my $output = do {
+      if (ref $self) {
+        $self->get_log()
+      } else {
+        my $log = shift;
+        $log;
+      }
+    };
+    confess 'ログの取得に失敗しました' unless defined $output;
     my $players = $self->extraction($output);
 
     no warnings 'utf8';
@@ -76,3 +84,51 @@ package Densetu::Tools::PlayerInfo {
 }
 
 1;
+
+=encoding utf8
+
+=head1 NAME
+
+Densetu::Tools::PlayerInfo - ログから武将情報を抽出して表示するツール
+
+=head1 SYNOPSIS
+
+  use Densetu::Tools::PlayerInfo;
+
+  # IDとPASSだけ指定するやり方
+  my $info = Densetu::Tools::PlayerInfo->new(
+    id => 'player_id',
+    pass => 'player_pass',
+  );
+  $info->output;
+
+  # ログだけ渡して抽出
+  my $log;
+  while (chomp(my $line = <STDIN>)) {
+    last if $line eq '';
+    $log .= "\n$line";
+  }
+  Densetu::Tools::PlayerInfo->output($log);
+
+=head1 ATTRIBUTES
+
+=head2 id
+
+抽出するログのプレイヤーID
+
+=head2 pass
+
+抽出するログのプレイヤーPASS
+
+=head1 METHODS
+
+=head2 output
+
+  $info->output;
+  # or
+  Densetu::Tools::PlayerInfo->output($log);
+
+ログを分析し、ログに含まれる武将情報をまとめて表示します。
+クラスメソッドとしても動作します。その場合はログ自体の情報をこのメソッドに渡す必要があります。
+
+=cut
